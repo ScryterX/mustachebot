5758bce600f9d2d34873877376ef956f59315f66
Josiel Cardoso <josielcardosomercio@gmail.com>
2024-07-26 12:36:48 -0300

    modify: optimize chat view

--- app/chat/components/Chat.tsx
+++ app/chat/components/Chat.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   Avatar,
   AvatarFallback,
@@ -24,59 +25,57 @@ interface ChatProps {}
 const AIChatComponent = (props: ChatProps) => {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   const { data } = useSession();
-
   return (
-    <div>
-      <Card className="w-[440px] grid-rows-[min-content_1fr_min-content]">
+    <div className="w-full max-w-full">
+      <Card className="grid-rows-[min-content_1fr_min-content]">
         <CardHeader>
           <CardTitle>Mustachebot</CardTitle>
           <CardDescription>Assistente virtual Mustachebarber</CardDescription>
         </CardHeader>
         <CardContent>
-          <ScrollArea className="h-[600px] w-full pr-4">
-            {messages.map((message) => {
-              return (
-                <div
-                  key={message.id}
-                  className="mt-2 flex gap-2 text-sm text-slate-600"
-                >
-                  {message.role === "user" && (
-                    <Avatar>
-                      <AvatarFallback>US</AvatarFallback>
-                      <AvatarImage src={data?.user?.image ?? undefined} />
-                    </Avatar>
-                  )}
+          <ScrollArea className="h-[400px] w-full pr-4 md:h-[600px]">
+            {messages.map((message) => (
+              <div
+                key={message.id}
+                className="mt-2 flex gap-2 text-sm text-slate-600"
+              >
+                {message.role === "user" && (
+                  <Avatar>
+                    <AvatarFallback>
+                      {data?.user?.name?.split(" ")[0][0]}
+                      {data?.user?.name?.split(" ")[1][0]}
+                    </AvatarFallback>
+                    <AvatarImage src={data?.user?.image ?? undefined} />
+                  </Avatar>
+                )}
 
-                  {message.role === "assistant" && (
-                    <Avatar>
-                      <AvatarFallback>MB</AvatarFallback>
-                      <AvatarImage src="https://static.vecteezy.com/ti/vetor-gratis/p1/4476066-isolado-macho-bigode-design-gratis-vetor.jpg" />
-                    </Avatar>
-                  )}
-                  <p className="leading-relaxed">
-                    <span className="block font-bold text-slate-700">
-                      {message.role === "user"
-                        ? (data?.user?.name ?? "Usuário")
-                        : "Mustachebot"}
-                    </span>
-                    {message.content}
-                  </p>
-                </div>
-              );
-            })}
+                {message.role === "assistant" && (
+                  <Avatar>
+                    <AvatarFallback>MB</AvatarFallback>
+                    <AvatarImage src="https://static.vecteezy.com/ti/vetor-gratis/p1/4476066-isolado-macho-bigode-design-gratis-vetor.jpg" />
+                  </Avatar>
+                )}
+                <p className="leading-relaxed">
+                  <span className="block font-bold text-slate-700">
+                    {message.role === "user"
+                      ? (data?.user?.name ?? "Usuário")
+                      : "Mustachebot"}
+                  </span>
+                  {message.content}
+                </p>
+              </div>
+            ))}
           </ScrollArea>
         </CardContent>
         <CardFooter>
-          <form
-            className="flex w-full justify-between gap-1"
-            onSubmit={handleSubmit}
-          >
+          <form className="flex w-full gap-2" onSubmit={handleSubmit}>
             <Input
               placeholder="Em que posso ajudar?"
               value={input}
               onChange={handleInputChange}
+              className="flex-1"
             />
-            <Button type="submit" variant={"outline"}>
+            <Button type="submit" variant={"outline"} className="flex-shrink-0">
               <SendIcon />
             </Button>
           </form>
